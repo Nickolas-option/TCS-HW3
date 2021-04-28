3a638b8a55724a8650af842cd6d4e625fbe82c90
Nickolas-option <69462766+Nickolas-option@users.noreply.github.com>
2021-04-28 15:55:20 +0300

    File parsing and E0 handling complete

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 with open(file="input.txt", mode="r") as inputFile:
-    line1, line2, line3, line4, line5 = inputFile.readlines()
-    if not (line1[:8] == "states=[" and line1[len(line1)-2:] == "]"):
+    with open(file="output.txt", mode="w") as outputFile:
+        # input parsing
+        line1, line2, line3, line4, line5 = inputFile.readlines()
 
-    states = line1[8: len(line1) - 2].split(',')
-    alpha = line2[7: len(line2) - 2].split(',')
-    initial = line3[9: len(line3) - 2].split(',')
-    accepting = line4[11: len(line4) - 2].split(',')
-    transitions = line5[7: len(line5) - 2].split(',')
+        if (line1[:8] == "states=[" and line1[len(line1) - 2:-1]) == "]":
+            states = line1[8: len(line1) - 2].split(',')
+        else:
+            outputFile.write("E0")
+        if line2[:7] == "alpha=[" and line2[len(line2) - 2:-1] == "]":
+            alpha = line2[7: len(line2) - 2].split(',')
+        else:
+            outputFile.write("E0")
+        if line3[:9] == "initial=[" and line3[len(line3) - 2:-1] == "]":
+            initial = line3[9: len(line3) - 2].split(',')
+        else:
+            outputFile.write("E0")
+        if line4[:11] == "accepting=[" and line4[len(line4) - 2:-1] == "]":
+            accepting = line4[11: len(line4) - 2].split(',')
+        else:
+            outputFile.write("E0")
+        if line5[:7] == "trans=[" and line5[len(line5) - 2:-1] == "]":
+            transitions = line5[7: len(line5) - 2].split(',')
+        else:
+            outputFile.write("E0")
+print(states, alpha, initial, accepting, transitions)
 
 # states=[on,off]
 # alpha=[turn_on,turn_off]
